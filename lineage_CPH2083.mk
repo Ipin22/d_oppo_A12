6e833964e423ec9b3266e94e494ae6180890b929
ARIPIN897 <kotakandis185@gmail.com>
2024-11-05 18:25:29 +0700

    initial skeleton tree

---
+++ lineage_CPH2083.mk
@@ -0,0 +1,35 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from device
+$(call inherit-product, device/oppo/CPH2083/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_BRAND := oppo
+PRODUCT_DEVICE := CPH2083
+PRODUCT_NAME := lineage_CPH2083
+PRODUCT_MANUFACTURER := oppo
+PRODUCT_MODEL := OPPO A12
+
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
+TARGET_VENDOR := oppo
+TARGET_VENDOR_PRODUCT_NAME := CPH2083
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="full_oppo6765_19451-user 9 PPR1.180610.011 eng.root.20200522.171412 release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := OPPO/CPH2083/OP4BFB:9/PPR1.180610.011/1572000930:user/release-keys
