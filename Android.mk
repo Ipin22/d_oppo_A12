4314f46e623517f1867ec50fc1acff126c23b23e
Ipin22 <ucihamala573@gmail.com>
2024-11-06 12:29:06 +0700

    CPH2083: fix build error
    missing endif

--- Android.mk
+++ Android.mk
@@ -13,6 +13,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
 LOCAL_PATH := $(call my-dir)
+
 ifeq ($(TARGET_DEVICE),CPH2083)
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
+endif
 
